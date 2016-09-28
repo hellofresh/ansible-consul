1d92e401f8bb557f9d9214dfad3f62c50d52d262
Adham Helal <aa@hellofresh.com>
2016-09-28 19:53:03 +0200

    travis tests

---
+++ test/ansible-setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+echo "Running travis "
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+SETUP_VERSION="v0.0.3"
+#SETUP_VERBOSITY="vv"
+
+## Install Ansible 2.0
+ANSIBLE_VERSIONS[0]="2.0.2.0"
+INSTALL_TYPE[0]="pip"
+ANSIBLE_LABEL[0]="v2"
+
+## Install Ansible stable-2.0
+ANSIBLE_VERSIONS[1]="devel"
+INSTALL_TYPE[1]="git"
+ANSIBLE_LABEL[1]="latest"
+
+# Whats the default version
+ANSIBLE_DEFAULT_VERSION="v2"
+
+## Create a temp dir
+filename=$( echo ${0} | sed 's|/||g' )
+my_temp_dir="$(mktemp -dt ${filename}.XXXX)"
+
+curl -s https://raw.githubusercontent.com/ahelal/avm/${SETUP_VERSION}/setup.sh -o $my_temp_dir/setup.sh
+
+## Run the setup
+. $my_temp_dir/setup.sh
