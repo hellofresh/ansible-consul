daf4b751fe61aab03a7f5c87ba905c52b3ea896d
Adham Helal <ahelal@users.noreply.github.com>
2016-11-10 21:20:36 +0100

    Update ansible-setup.sh

--- test/ansible-setup.sh
+++ test/ansible-setup.sh
@@ -7,14 +7,13 @@ SETUP_VERSION="v0.0.3"
 #SETUP_VERBOSITY="vv"
 
 ## Install Ansible 2.0
-ANSIBLE_VERSIONS[0]="2.0.2.0"
+ANSIBLE_VERSIONS[0]="2.1.0.0"
 INSTALL_TYPE[0]="pip"
-ANSIBLE_LABEL[0]="v2"
+ANSIBLE_LABEL[0]="v2.1"
 
-## Install Ansible stable-2.0
-ANSIBLE_VERSIONS[1]="devel"
-INSTALL_TYPE[1]="git"
-ANSIBLE_LABEL[1]="latest"
+ANSIBLE_VERSIONS[1]="2.2.0.0"
+INSTALL_TYPE[1]="pip"
+ANSIBLE_LABEL[1]="v2.2"
 
 # Whats the default version
 ANSIBLE_DEFAULT_VERSION="v2"
