60c039141d001f25d14bb68e1188061764b15fab
Adham Helal <aa@hellofresh.com>
2016-11-10 23:22:25 +0100

    Various fixes for 2.2

--- test/ansible-setup.sh
+++ test/ansible-setup.sh
@@ -7,16 +7,16 @@ SETUP_VERSION="v0.0.3"
 #SETUP_VERBOSITY="vv"
 
 ## Install Ansible 2.0
-ANSIBLE_VERSIONS[0]="2.1.0.0"
+ANSIBLE_VERSIONS[0]="2.0.2.0"
 INSTALL_TYPE[0]="pip"
-ANSIBLE_LABEL[0]="v2.1"
+ANSIBLE_LABEL[0]="v2.0"
 
 ANSIBLE_VERSIONS[1]="2.2.0.0"
 INSTALL_TYPE[1]="pip"
 ANSIBLE_LABEL[1]="v2.2"
 
 # Whats the default version
-ANSIBLE_DEFAULT_VERSION="v2"
+ANSIBLE_DEFAULT_VERSION="v2.0"
 
 ## Create a temp dir
 filename=$( echo ${0} | sed 's|/||g' )
