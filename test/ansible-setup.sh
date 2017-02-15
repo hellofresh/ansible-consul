c1f1e6ca8819dcca986bbfee53a6a4259bc27249
Adham Helal <aa@hellofresh.com>
2017-02-15 18:37:51 +0100

    update testing infra

--- test/ansible-setup.sh
+++ test/ansible-setup.sh
@@ -9,15 +9,14 @@ SETUP_VERSION="v0.0.3"
 ## Install Ansible 2.0
 ANSIBLE_VERSIONS[0]="2.0.2.0"
 INSTALL_TYPE[0]="pip"
-ANSIBLE_LABEL[0]="v2"
+ANSIBLE_LABEL[0]="v2.0"
 
-## Install Ansible stable-2.0
-ANSIBLE_VERSIONS[1]="devel"
-INSTALL_TYPE[1]="git"
-ANSIBLE_LABEL[1]="latest"
+ANSIBLE_VERSIONS[1]="2.2.0.0"
+INSTALL_TYPE[1]="pip"
+ANSIBLE_LABEL[1]="v2.2"
 
 # Whats the default version
-ANSIBLE_DEFAULT_VERSION="v2"
+ANSIBLE_DEFAULT_VERSION="v2.0"
 
 ## Create a temp dir
 filename=$( echo ${0} | sed 's|/||g' )
