cf331fba99bce8a0f8c2bc5a9ea639e8e4cdc479
Adham Helal <aa@hellofresh.com>
2017-02-23 10:58:39 +0100

    Formating

--- test/integration/basic-agent/serverspec/consul_agent_spec.rb
+++ test/integration/basic-agent/serverspec/consul_agent_spec.rb
@@ -11,7 +11,7 @@ describe 'consul agent' do
     it { should belong_to_group 'consul' }
   end
 
-  %w(/opt/consul /opt/consul/bin /opt/consul/data /etc/consul.d ).each do |dir|
+  %w(/opt/consul /opt/consul/bin /opt/consul/data /etc/consul.d).each do |dir|
     describe file(dir) do
       it { should be_directory }
       it { should be_owned_by('consul') }
@@ -26,7 +26,7 @@ describe 'consul agent' do
   describe file('/var/log/consul/consul-agent.log') do
     it { should be_file }
     it { should be_owned_by('consul') }
-    its (:content) { should contain "New leader elected:" }
+    its (:content) { should contain 'New leader elected:' }
   end
 
   describe port(8300) do
@@ -45,7 +45,7 @@ describe 'consul agent' do
   end
 
   describe 'UI should be disabled' do
-    describe command "curl -s -I http://127.0.0.1:8500/ui/" do
+    describe command 'curl -s -I http://127.0.0.1:8500/ui/' do
       its(:exit_status) { should eq 0 }
       its(:stdout) { should contain 'HTTP/1.1 404 Not Found' }
     end
--- test/integration/basic-agent/serverspec/haproxy_spec.rb
+++ test/integration/basic-agent/serverspec/haproxy_spec.rb
@@ -1,35 +1,33 @@
 require_relative '../../helper_spec.rb'
 
-
 describe 'HAPROXY' do
+  describe service('haproxy') do
+    it { should be_enabled }
+    it { should be_running }
+  end
 
-    describe service('haproxy') do
-      it { should be_enabled }
-      it { should be_running }
+  describe file('/etc/haproxy/haproxy.cfg') do
+    it { should be_file }
+    it { should be_mode 644 }
+    it { should be_owned_by 'consul' }
+    it { should be_grouped_into 'consul' }
+  end
+
+  describe file('/etc/haproxy/') do
+    it { should be_directory }
+    it { should be_mode 775 }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'consul' }
+  end
+
+  describe 'stats page' do
+    describe port(3212) do
+      it { should be_listening.with('tcp') }
     end
 
-    describe file('/etc/haproxy/haproxy.cfg') do
-      it { should be_file }
-      it { should be_mode 644 }
-      it { should be_owned_by 'consul' }
-      it { should be_grouped_into 'consul' }
+    describe command 'curl -s http://127.0.0.1:3212' do
+      its(:exit_status) { should eq 0 }
+      its(:stdout) { should contain 'Statistics Report' }
     end
-
-    describe file('/etc/haproxy/') do
-      it { should  be_directory}
-      it { should be_mode 775 }
-      it { should be_owned_by 'root' }
-      it { should be_grouped_into 'consul' }
-    end
-
-    describe "stats page" do
-        describe port(3212) do
-            it { should be_listening.with('tcp') }
-        end
-
-        describe command "curl -s http://127.0.0.1:3212" do
-            its(:exit_status) { should eq 0 }
-            its(:stdout) { should contain 'Statistics Report' }
-        end
-    end
+  end
 end
