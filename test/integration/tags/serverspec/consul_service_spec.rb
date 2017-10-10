9150daf08a29ed3dba6089782f90150dcd903dd7
Ali Rizwan <ari@hellofresh.com>
2017-10-10 16:58:50 +0200

    Only use backends which have the same environment.
    All services created now have a tag {{ envrionment_name }} (default 'staging').
    Updated haproxy config to only use servcies that have the same envrionment tag.
    
    Signed-off-by: Ali Rizwan <ari@hellofresh.com>

--- test/integration/tags/serverspec/consul_service_spec.rb
+++ test/integration/tags/serverspec/consul_service_spec.rb
@@ -5,7 +5,10 @@ describe 'hellofresh service' do
   describe command 'curl -s -v http://127.0.0.1:8500/v1/health/service/hellofresh' do
     its(:exit_status) { should eq 0 }
     its(:stdout) { should contain '"ServiceName":"hellofresh"' }
-    its(:stdout) { should contain '"Tags":\["test","v1.1.5","WEIGHT:77"]' }
+    its(:stdout) { should match /"Tags":\[.*"test".*/ }
+    its(:stdout) { should match /"Tags":\[.*"v1\.1\.5".*/ }
+    its(:stdout) { should match /"Tags":\[.*"WEIGHT:77".*/ }
+    its(:stdout) { should match /"Tags":\[.*"staging".*/ }
     its(:stdout) { should contain '"Port":80' }
     its(:stdout) { should contain '"Status":"passing"' }
   end
@@ -15,7 +18,9 @@ describe 'superssh-different-name service' do
   describe command 'curl -s -v http://127.0.0.1:8500/v1/health/service/superssh-different-name' do
     its(:exit_status) { should eq 0 }
     its(:stdout) { should contain '"Service":"superssh-different-name"' }
-    its(:stdout) { should contain '"Tags":\["test","v2.1.2"]' }
+    its(:stdout) { should match /"Tags":\[.*"staging".*/ }
+    its(:stdout) { should match /"Tags":\[.*"test".*/ }
+    its(:stdout) { should match /"Tags":\[.*"v2\.1\.2".*/ }
     its(:stdout) { should contain '"Port":22' }
     its(:stdout) { should contain '"Status":"passing"' }
   end
@@ -25,7 +30,9 @@ describe 'superdb' do
   describe command 'curl -s -v http://127.0.0.1:8500/v1/health/service/superdb' do
     its(:exit_status) { should eq 0 }
     its(:stdout) { should contain '"Service":"superdb"' }
-    its(:stdout) { should contain '"Tags":\["test","v3.9.2"]' }
+    its(:stdout) { should match /"Tags":\[.*"staging".*/ }
+    its(:stdout) { should match /"Tags":\[.*"test".*/ }
+    its(:stdout) { should match /"Tags":\[.*"v3\.9\.2".*/ }
     its(:stdout) { should contain '"Port":2122' }
     its(:stdout) { should contain '"Status":"(warning|critical)"' }
   end
